8cf22f9098315ed2d14de39dcc5eeba13672e308
Kaustubh <saikaustubh10@gmail.com>
2021-03-01 12:24:55 +0530

    basic setup complete

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import SearchBar from './SearchBar';
+
+class App extends React.Component{
+    render() {
+        return (
+            <div><SearchBar/></div>
+        );
+    }
+}
+
+export default App;
