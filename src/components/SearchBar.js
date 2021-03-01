711430f9189fd5d47fd5f204774740fd0e77d7ce
Kaustubh <saikaustubh10@gmail.com>
2021-03-01 16:06:09 +0530

    added api instance

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
 class SearchBar extends React.Component{
+
+    state = {term : ''};
+
+    onInputChange = (event) => {
+        this.setState({ term: event.target.value });
+    };
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+
+        // TODO: Make sure we call a callback from parent
+    };
+
     render(){
         return (
-            <div>Search bar</div>
+            <div className="search-bar ui segment">
+                <form className="ui form" onSubmit={this.onFormSubmit}>
+                    <div className="field">
+                        <label><i className="search icon"></i> Video Search</label>
+                        <input type="text" value={this.state.term} onChange={this.onInputChange}/>
+                    </div>
+                </form>
+            </div>
         );
     }
 }
