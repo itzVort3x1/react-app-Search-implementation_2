cb40409403f45013061fe0a8ea9be4776e2bf7b9
Kaustubh <saikaustubh10@gmail.com>
2021-03-02 12:58:50 +0530

    completed code

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -4,7 +4,7 @@ import './VideoItem.css';
 const VideoItem = ({ video, onVideoSelect }) => {
     return (
         <div onClick={() => onVideoSelect(video)} className="item video-item">
-            <img className="ui image" src={video.snippet.thumbnails.medium.url} alt="img"/>
+            <img className="ui image" src={video.snippet.thumbnails.medium.url} alt={video.snippet.title}/>
             <div className="content">
                 <div className="header">{video.snippet.title}</div>
             </div>
